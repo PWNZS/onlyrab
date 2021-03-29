b6fafbd25740b5adc9fb9f748e73abb6554c26cd
Must Die <46817781+hudoils@users.noreply.github.com>
2021-03-29 17:47:26 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -6,17 +6,18 @@ from random import choice, randint, random
 from threading import Thread
 from time import sleep, strftime
 
+from fake_useragent import UserAgent
 from requests import get, post
 
 
 def buy_slave(id):
-    """Покупает раба. vk.com/onlyrab"""
+    """Покупает раба."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buySlave",
         headers={
             "Content-Type": "application/json",
             "authorization": auth,
-            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "User-agent": ua,
             "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
         },
         json={"slave_id": id},
@@ -24,40 +25,27 @@ def buy_slave(id):
 
 
 def buy_fetter(id):
-    """Покупает оковы. vk.com/onlyrab"""
+    """Покупает оковы."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/buyFetter",
         headers={
             "Content-Type": "application/json",
             "authorization": auth,
-            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "User-agent": ua,
             "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
         },
         json={"slave_id": id},
     )
 
 
-def get_start():
-    """Получает полную информацию о своём профиле. vk.com/onlyrab"""
-    return get(
-        "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/start",
-        headers={
-            "Content-Type": "application/json",
-            "authorization": auth,
-            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
-            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
-        },
-    ).json()
-
-
 def sell_slave(id):
-    """Продаёт раба. vk.com/onlyrab"""
+    """Продаёт раба."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/saleSlave",
         headers={
             "Content-Type": "application/json",
             "authorization": auth,
-            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "User-agent": ua,
             "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
         },
         json={"slave_id": id},
@@ -65,13 +53,13 @@ def sell_slave(id):
 
 
 def job_slave(id):
-    """Даёт работу. vk.com/onlyrab"""
+    """Даёт работу."""
     post(
         "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/jobSlave",
         headers={
             "Content-Type": "application/json",
             "authorization": auth,
-            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "User-agent": ua,
             "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
         },
         json={
@@ -82,65 +70,190 @@ def job_slave(id):
 
 
 def get_user(id):
-    """Получает информацие о пользователе. vk.com/onlyrab"""
+    """Получает информацие о пользователе."""
     return get(
-        "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/user",
+        f"https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/user?id={id}",
         headers={
             "Content-Type": "application/json",
             "authorization": auth,
-            "User-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.105 Safari/537.36",
+            "User-agent": ua,
             "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
         },
-        params={"id": id},
     ).json()
 
 
+def get_slave_list(id):
+    """Возвращает список рабов пользователя."""
+    return get(
+        f"https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/slaveList?id={id}",
+        headers={
+            "Content-Type": "application/json",
+            "authorization": auth,
+            "User-agent": ua,
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
+    ).json()
+
+
+def get_top_users():
+    """Возвращает список топ игроков."""
+    return get(
+        "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/topUsers",
+        headers={
+            "Content-Type": "application/json",
+            "authorization": auth,
+            "User-agent": ua,
+            "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+        },
+    ).json()
+
+
+def get_start():
+    """Получает полную информацию о своём профиле."""
+    while True:
+        try:
+            global good_start
+            start = get(
+                "https://pixel.w84.vkforms.ru/HappySanta/slaves/1.0.0/start",
+                headers={
+                    "Content-Type": "application/json",
+                    "authorization": auth,
+                    "User-agent": ua,
+                    "origin": "https://prod-app7794757-c1ffb3285f12.pages-ac.vk-apps.com",
+                },
+            ).json()
+            if "slaves" in start.keys():
+                good_start = start
+            sleep(delay + random())
+        except Exception as e:
+            print(e.args)
+            sleep(delay + random())
+
+
+def upgrade_slave(me, slave_id):
+    """Прокачивает раба, чтобы он приносил 1000 в минуту."""
+    # Проверка на то, дал ли нормальный сервер нормальный ответ
+    if "balance" in me.keys():
+        # Проверка на то, хватит ли баланса для прокачки
+        if int(me["balance"]) >= 39214:
+            try:
+                slave_price = int(get_user(slave_id)["price"])
+                while slave_price <= 26151:
+                    sell_slave(slave_id)
+                    print(f"Продал id{slave_id} для улучшения")
+                    buy_slave(slave_id)
+                    print(f"Улучшил id{slave_id}")
+                    sleep(delay + random())
+                    slave_price = int(get_user(slave_id)["price"])
+            except Exception as e:
+                print(e.args)
+                sleep(delay + random())
+                pass
+
+
+def upgrade_slaves():
+    """Прокачивает рабов, чтобы они приносили 1000 в минуту."""
+    while True:
+        try:
+            # Перебор списка рабов
+            for slave in good_start["slaves"]:
+                balance = get_user(my_id)["balance"]
+                if int(balance) >= 39214:
+                    slave_price = get_user(slave["id"])["price"]
+                    while int(slave_price) <= 26151:
+                        sell_slave(slave["id"])
+                        print(f"Продал id{slave['id']} для улучшения")
+                        buy_slave(slave["id"])
+                        print(f"Улучшил id{slave['id']}")
+                        sleep(delay + random())
+                        slave_price = get_user(slave["id"])["price"]
+                    balance = get_user(my_id)["balance"]
+        except Exception as e:
+            print(e.args)
+            sleep(delay + random())
+
+
+def buy_top_users_slaves():
+    """То же самое, что и buy_slaves, только перекупает рабов у топ игроков."""
+    while True:
+        try:
+            top_users = get_top_users()
+            if "list" in top_users.keys():
+                for top_user in top_users["list"]:
+                    top_user_slaves = get_slave_list(int(top_user["id"]))
+                    if "slaves" in top_user_slaves.keys():
+                        for slave in top_user_slaves["slaves"]:
+                            if int(slave["fetter_to"]) == 0:
+                                slave_id = slave["id"]
+                                slave_info = get_user(slave_id)
+                                if slave_info["price"] <= max_price:
+                                    # Покупка раба
+                                    buy_slave(slave_id)
+
+                                    # Получение информации о себе
+                                    me = get_user(my_id)
+
+                                    print(
+                                        f"""\n==[{strftime("%d.%m.%Y %H:%M:%S")}]==
+Купил id{slave_info["id"]} за {slave_info["price"]} у id{top_user["id"]}
+Баланс: {"{:,}".format(me['balance'])}
+Рабов: {"{:,}".format(me['slaves_count'])}
+vk.com/onlyrab- Доход в минуту: {"{:,}".format(me['slaves_profit_per_min'])}
+Место в рейтинге: {"{:,}".format(me['rating_position'])}\n""",
+                                    )
+
+                                    # Прокачивает раба
+                                    if conf_upgrade_slaves == 1:
+                                        upgrade_slave(me, slave_id)
+
+                                    # Покупает оковы только что купленному рабу
+                                    if buy_fetters == 1:
+                                        buy_fetter(slave_id)
+                                        print(
+                                            f"Купил оковы vk.com/id{slave_id}"
+                                        )
+                                    sleep(delay + random())
+        except Exception as e:
+            print(e.args)
+            sleep(delay + random())
+
+
 def buy_slaves():
-    """Покупает и улучшает рабов, надевает оковы, если включено в config.json. vk.com/onlyrab"""
+    """Покупает и улучшает рабов, надевает оковы, если включено в config.json."""
     while True:
         try:
             # Случайный раб в промежутке
-            rand_slave = randint(1, 646735737)
-            rand_slave_info = get_user(rand_slave)
+            slave_id = randint(1, 646959225)
+            slave_info = get_user(slave_id)
 
             # Проверка раба на соотвествие настройкам цены
-            while not (
-                int(rand_slave_info["price"]) <= max_price
-                and int(rand_slave_info["price"]) >= min_price
-            ):
-                rand_slave = randint(1, 646735737)
-                rand_slave_info = get_user(rand_slave)
+            while int(slave_info["price"]) >= max_price:
+                slave_id = randint(1, 646959225)
+                slave_info = get_user(slave_id)
 
             # Покупка раба
-            buy_slave(rand_slave)
+            buy_slave(slave_id)
 
             # Получение информации о себе
             me = get_user(my_id)
 
             print(
                 f"""\n==[{strftime("%d.%m.%Y %H:%M:%S")}]==
-Купил vk.com/id{rand_slave} за {rand_slave_info["price"]}
+Купил id{slave_info["id"]} за {slave_info["price"]}
 Баланс: {"{:,}".format(me['balance'])}
 Рабов: {"{:,}".format(me['slaves_count'])}
-vk.com/onlyrab - Доход в минуту: {"{:,}".format(me['slaves_profit_per_min'])}
+vk.com/onlyrab- Доход в минуту: {"{:,}".format(me['slaves_profit_per_min'])}
 Место в рейтинге: {"{:,}".format(me['rating_position'])}\n""",
             )
-            if upgrade_slaves == 1:
-                # Перебор списка рабов
-                if "balance" in me.keys():
-                    # Проверка на то, хватит ли баланса для прокачки
-                    if int(me["balance"]) >= 39214:
-                        while int(get_user(rand_slave)["price"]) <= 26151:
-                            sell_slave(rand_slave)
-                            print("Продал раба для улучшения vk.com/onlyrab")
-                            buy_slave(rand_slave)
-                            print("Улучшил раба vk.com/onlyrab")
-                            sleep(delay + random())
+
+            # Прокачивает раба
+            if conf_upgrade_slaves == 1:
+                upgrade_slave(me, slave_id)
 
             # Покупает оковы только что купленному рабу
             if buy_fetters == 1:
-                buy_fetter(rand_slave)
-                print(f"Купил оковы vk.com/id{rand_slave}")
+                buy_fetter(slave_id)
+                print(f"Купил оковы vk.com/id{slave_id}")
 
             sleep(delay + random())
         except Exception as e:
@@ -149,18 +262,21 @@ vk.com/onlyrab - Доход в минуту: {"{:,}".format(me['slaves_profit_pe
 
 
 def buy_fetters():
-    """Покупает оковы тем, у кого их нет. vk.com/onlyrab"""
+    """Покупает оковы тем, у кого их нет."""
     while True:
         try:
-            # Получение полной информации об аккаунте
-            start = get_start()
+            slaves = good_start["slaves"]
+
+            # Удаление первого раба из списка, чтобы не происходило коллизии с прокачкой
+            if conf_upgrade_slaves == 1:
+                del slaves[0]
 
             # Перебор списка рабов
-            for slave in start["slaves"]:
+            for slave in slaves:
                 # Проверка на наличие оков
                 if int(slave["fetter_to"]) == 0:
                     buy_fetter(slave["id"])
-                    print(f"Купил оковы vk.com/id{slave['id']}")
+                    print(f"Купил оковы id{slave['id']}")
                     sleep(delay + random())
         except Exception as e:
             print(e.args)
@@ -168,18 +284,18 @@ def buy_fetters():
 
 
 def job_slaves():
-    """Даёт безработным работу. vk.com/onlyrab"""
+    """Даёт безработным работу."""
     while True:
         try:
-            # Получение полной информации об аккаунте
-            start = get_start()
-
+            slaves = good_start["slaves"]
+            if conf_buy_slaves == 0 and conf_buy_fetters == 1:
+                del slaves[0]
             # Перебор списка рабов
-            for slave in start["slaves"]:
+            for slave in good_start["slaves"]:
                 # Проверка на наличие у раба работы
                 if not slave["job"]["name"]:
-                    job_slave(slave["id"])
-                    print(f"Дал работу vk.com/id{slave['id']}")
+                    job_slave(int(slave["id"]))
+                    print(f"Дал работу id{slave['id']}")
                     sleep(delay + random())
         except Exception as e:
             print(e.args)
@@ -196,24 +312,40 @@ if __name__ == "__main__":
         try:
             config = load(f)
         except:
-            print("Неверный конфиг vk.com/onlyrab")
+            print("Неверный конфиг")
             sys.exit()
     auth = str(config["authorization"])
     conf_buy_fetters = int(config["buy_fetters"])
     conf_buy_slaves = int(config["buy_slaves"])
     delay = int(config["delay"])
+    top_hate = int(config["top_hate"])
     try:
         job = list(config["job"])
     except:
         job = str(config["job"])
     max_price = int(config["max_price"])
-    min_price = int(config["min_price"])
     my_id = int(config["my_id"])
-    upgrade_slaves = int(config["upgrade_slaves"])
+    conf_upgrade_slaves = int(config["upgrade_slaves"])
 
-    # Запуск
-    if conf_buy_slaves == 1:
+    # Нужная глобальные переменные
+    good_start = {}
+
+    # Создание фейкового UserAgent для избежания бана
+    ua = UserAgent(cache=False).random
+
+    Thread(target=get_start).start()
+
+    # Задержка, чтобы успеть получить start
+    sleep(3)
+
+    if conf_buy_slaves == 1 and top_hate == 0:
+        print("Включена покупка случайных рабов.")
         Thread(target=buy_slaves).start()
+    elif conf_buy_slaves == 1 and top_hate == 1:
+        print("Включена перекупка рабов у топеров.")
+        Thread(target=buy_top_users_slaves).start()
+    if conf_upgrade_slaves == 1 and conf_buy_slaves == 0:
+        Thread(target=upgrade_slaves).start()
     if conf_buy_fetters == 1:
         Thread(target=buy_fetters).start()
     Thread(target=job_slaves).start()
